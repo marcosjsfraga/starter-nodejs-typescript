d136c04bdc11fcd5f512bb3db9e972f387105717
Marcos CKL <marcosfraga@ckl.io>
2021-12-16 13:30:03 -0300

    ✅ create test files

---
+++ src/api/auth/useCases/session/SessionUseCase.tests.ts
@@ -0,0 +1,8 @@
+// const EMAIL = 'john.doe@example.com'
+// const PASSWORD = '123456'
+
+describe('Session', () => {
+  it('should receive JWT token when authenticated with valid credencials', async () => {
+    expect(true).toBeTruthy()
+  })
+})
---
+++ src/api/auth/useCases/signup/SignupUseCase.tests.ts
@@ -0,0 +1,13 @@
+// const EMAIL = 'john.doe@example.com'
+// const PASSWORD = '123456'
+// const NAME = 'John Doe'
+// const BIRTHDAY = '01/01/2000'
+
+describe('Signup', () => {
+  it('should be able to signup successfully', () => {
+    const sum = 2 + 2
+    const result = 4
+
+    expect(sum).toBe(result)
+  })
+})
