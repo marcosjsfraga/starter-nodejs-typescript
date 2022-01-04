9fb0ba97174fa2e18085cca22fd3959ca71465b9
Marcos CKL <marcosfraga@ckl.io>
2022-01-04 11:18:39 -0400

    ♻️ Revert referenced imports

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
 import { createConnection } from 'typeorm'
 
 createConnection()
+
+// import { Connection, createConnection, getConnectionOptions } from 'typeorm'
+
+// export default async (host = 'database'): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions()
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host,
+//       database: process.env.NODE_ENV === 'test' ? 'starter_test' : defaultOptions.database,
+//     })
+//   )
+// }
