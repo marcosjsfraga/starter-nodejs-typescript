f8aab3dfc4b1995dfa7ed5e2ed15db0b8fae7fbb
Marcos CKL <marcosfraga@ckl.io>
2022-01-07 08:54:50 -0400

    ♻️ refactoring auth integrations tests

--- src/api/auth/useCases/signup/SignupUseCase.tests.ts
+++ src/api/auth/useCases/signup/SignupUseCase.tests.ts
@@ -1,15 +1,26 @@
 /* eslint-disable no-console */
 import request from 'supertest'
-import 'database'
+// import { createConnection } from 'typeorm'
+
 import application from 'interfaces/express'
 
-const NAME = 'John Doe'
-const EMAIL = 'john.doe@example.com'
+const NAME = 'John1 Doe'
+const EMAIL = 'john1.doe@example.com'
 const PASSWORD = '123456'
 const BIRTHDAY = '01/01/2000'
 
 describe('Signup', () => {
+  // beforeEach(async () => {
+  //   await createConnection()
+  // })
+
+  // afterEach(async () => {
+  //   console.log('...afterEach')
+  // })
+
   it('should be able to signup successfully', async () => {
+    // console.log('app_tests', app_tests)
+
     const response = await request(application).post('/signup').send({
       name: NAME,
       email: EMAIL,
@@ -17,7 +28,7 @@ describe('Signup', () => {
       birthday: BIRTHDAY,
     })
 
-    console.log(response.body)
+    console.log('- BODY -', response.body)
 
     expect(response.statusCode).toEqual(200)
   })
